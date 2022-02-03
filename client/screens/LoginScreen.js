a372e73f1f0c661bc2d6987d3e91ac7fa34e1420
Kevin Dao <kevin-dao@uiowa.edu>
2022-02-02 19:42:27 -0600

    Add initial UI design for login screen

--- client/screens/LoginScreen.js
+++ client/screens/LoginScreen.js
@@ -1,10 +1,40 @@
-import { View, Text } from 'react-native';
+import { View, Text, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native';
 import React from 'react';
+import tw from 'tailwind-react-native-classnames';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function LoginScreen() {
   return (
-    <View>
-      <Text>Login screen</Text>
-    </View>
+    <KeyboardAvoidingView
+      behavior='padding'
+      style={tw`flex-1 justify-center items-center`}
+    >
+      <SafeAreaView style={tw`w-4/5`}>
+        <Text style={tw`text-3xl font-bold text-gray-900 mb-5 text-center`}>Login</Text>
+        <TextInput
+          style={tw`bg-white rounded p-2 my-1 border border-gray-300`}
+          placeholder='Email'
+        />
+        
+        <TextInput
+          style={tw`bg-white rounded p-2 my-1 border border-gray-300`}
+          placeholder='Password'
+          secureTextEntry
+        />
+      </SafeAreaView>
+
+      <View style={tw`bg-blue-200 rounded p-2 mb-2 w-1/2`}>
+        <TouchableOpacity>
+          <Text style={tw`text-center`}>Login</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={tw`bg-blue-200 rounded p-2 mb-2 w-1/2`}>
+        <TouchableOpacity style={tw``}>
+          <Text style={tw`text-center`}>Register</Text>
+        </TouchableOpacity>
+      </View>
+      
+    </KeyboardAvoidingView>
   );
 }
