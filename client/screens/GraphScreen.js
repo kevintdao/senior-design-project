131571f84a4b1932bbd757015188ca7fa48fcf54
Kevin Dao <kevin-dao@uiowa.edu>
2022-03-06 22:48:43 -0600

    Add initial geolocation map for graphing data

--- client/screens/GraphScreen.js
+++ client/screens/GraphScreen.js
@@ -1,10 +1,31 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Dimensions } from 'react-native'
 import React from 'react'
+import { useAuth } from '../AuthContext'
+import MapView from 'react-native-maps'
 
 export default function GraphScreen() {
+  const { currentUser } = useAuth();
+
   return (
     <View>
+      <MapView 
+        style={styles.map} 
+        initialRegion={{
+          latitude: 41.65944687412238,
+          longitude: -91.53652901001102,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.04,
+        }}
+      />
       <Text>GraphScreen</Text>
     </View>
+
   )
 }
+
+const styles = StyleSheet.create({
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height / 2,
+  },
+})
