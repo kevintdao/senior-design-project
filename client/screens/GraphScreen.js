4dbaad8d8a9aec698e953ad15f7ca695b1727c76
Kevin Dao <kevin-dao@uiowa.edu>
2022-03-07 16:15:08 -0600

    Add initial Sessions component

--- client/screens/GraphScreen.js
+++ client/screens/GraphScreen.js
@@ -2,9 +2,37 @@ import { View, Text, StyleSheet, Dimensions } from 'react-native'
 import React from 'react'
 import { useAuth } from '../AuthContext'
 import MapView from 'react-native-maps'
+import { query, collection, getDocs } from '@firebase/firestore'
+import { db } from '../utils/firebase'
+import tw from 'twrnc'
+import Session from '../components/Sessions'
 
 export default function GraphScreen() {
   const { currentUser } = useAuth();
+  const user = currentUser.user;
+  const email = user.email;
+
+  const testData = {
+    'session_1': {
+      start: new Date()
+    },
+    'session_2': {
+      start: new Date()
+    }
+  }
+
+  const getData = async () => {
+    const output = [];
+    const q = query(collection(db, `users/${email}/sessions`));
+
+    // const docsSnap = await getDocs(q);
+
+    // docsSnap.forEach((doc) => {
+    //   console.log(doc.id);
+    // });
+  } 
+
+  getData();
 
   return (
     <View>
@@ -17,7 +45,10 @@ export default function GraphScreen() {
           longitudeDelta: 0.04,
         }}
       />
-      <Text>GraphScreen</Text>
+
+      <Text style={tw`text-3xl font-bold text-gray-900 mt-1 mb-2 text-center`}>Sessions</Text>
+
+      <Session data={testData}/>
     </View>
 
   )
