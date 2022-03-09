3483426d1dec594d1475d7bb0a1ee160c064ca36
Kevin Dao <kevin-dao@uiowa.edu>
2022-03-08 21:25:55 -0600

    Add time column

--- client/components/DataList.jsx
+++ client/components/DataList.jsx
@@ -2,6 +2,7 @@ import { View, Text, FlatList } from 'react-native'
 import React from 'react'
 import { DataTable } from 'react-native-paper'
 import tw from 'twrnc'
+import { formatTime } from '../utils/time'
 
 export default function DataList(props) {
   const data = props.data;
@@ -10,15 +11,20 @@ export default function DataList(props) {
     <View>
       <DataTable>
         <DataTable.Header>
-          <DataTable.Title>Marker Number</DataTable.Title>
+          <DataTable.Title>Marker</DataTable.Title>
           <DataTable.Title>Temperature</DataTable.Title>
+          <DataTable.Title>Time</DataTable.Title>
         </DataTable.Header>
 
         {data.map((item, index) => {
+          const timestamp = item.time.toDate();;
+          const time = formatTime(timestamp.getHours(), timestamp.getMinutes());
+
           return (
             <DataTable.Row key={index}>
               <DataTable.Cell>{index + 1}</DataTable.Cell>
               <DataTable.Cell>{item.temperature}</DataTable.Cell>
+              <DataTable.Cell>{`${time}`}</DataTable.Cell>
             </DataTable.Row>
           )
         })}
