b81febe23726aa03f4fd446785809b5be89f5c09
afilerio <alfredofilerio@gmail.com>
2022-03-10 13:02:33 -0600

    updated start session screen

--- client/screens/StartSessionScreen.js
+++ client/screens/StartSessionScreen.js
@@ -1,20 +1,28 @@
 import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import tw from 'twrnc';
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
+import {app} from '../utils/firebase';
+import firestore from '@react-native-firebase/firestore';
+
 const StartSessionScreen = () => {
   const navigation = useNavigation();
+  const markerList = [];
+  const handleSubmit = () => {
+    // submit markers in list
+    firestore().collection('Markers').add(markerList);
+  }
+  function handlePress(e) {
+    markerList.push(e.nativeEvent.coordinate);
+    return <Marker coordinate= {e.nativeEvent.coordinate}/>;
+  }
+
   return (
     <SafeAreaView style={tw`flex-1 items-center bg-blue-200`}>
         <View>
           <Text style={tw`text-black`}>Session</Text>
-        </View>
-        <View style={tw`bg-green-600 mt-3 items-center rounded-md p-2`}> 
-          <TouchableOpacity>
-            <Text style={tw`text-white`}>Begin New Session</Text>
-          </TouchableOpacity>
         </View>
         <View>
           <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')}>
@@ -22,8 +30,14 @@ const StartSessionScreen = () => {
           </TouchableOpacity>
         </View>
         <View>
-          <MapView style={styles.map} />
+          <MapView style={styles.map} onDoublePress = {handlePress} />
         </View>
+        <View style={tw`bg-green-600 mt-3 items-center rounded-md p-2`}> 
+          <TouchableOpacity onPress={handleSubmit}>
+            <Text style={tw`text-white`}>Begin New Session</Text>
+          </TouchableOpacity>
+        </View>
+
     </SafeAreaView>
 
   )
