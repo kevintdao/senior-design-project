04545118cafde5d5f81f63500afd3ef1c3793c1a
Kevin Dao <kevin-dao@uiowa.edu>
2022-04-13 18:06:40 -0500

    Add initial route for sending data

--- server/index.js
+++ server/index.js
@@ -5,14 +5,18 @@ var admin = require("firebase-admin");
 var serviceAccount = require("./service-account.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://senior-design-project-ceb4f-default-rtdb.firebaseio.com/'
 });
+
+var firestoreDB = admin.firestore()
+var realtimeDB = admin.database()
 
 const app = express()
 
 const server = require('http').createServer(app)
 const bodyParser = require('body-parser')
-const WebSocket = require('ws')
+const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ server: server })
 const port = process.env.PORT || 3000
@@ -22,9 +26,19 @@ app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send("Hello World")
+  // console.log(firestoreDB.collection('users').doc('test@test.com').get().then(doc => {
+  //   console.log(doc.data())
+  // }))
+  const ref = realtimeDB.ref('/')
+  ref.on('value', snapshot => {
+    console.log(snapshot.val())
+  })
+})
+
+app.post('/send_data', (req, res) => {
+
 })
 
 server.listen(port, () => {
   console.log(`Listening on port ${port}`)
-  console.log(admin)
 })
