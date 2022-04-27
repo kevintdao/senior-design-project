22bcb038320e5ee52487193da3a2bccea13639c1
Kevin Dao <kevin-dao@uiowa.edu>
2022-04-26 19:56:44 -0500

    Update

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ var admin = require("firebase-admin");
 
 var serviceAccount = require("./service-account.json");
 
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://senior-design-project-ceb4f-default-rtdb.firebaseio.com/'
